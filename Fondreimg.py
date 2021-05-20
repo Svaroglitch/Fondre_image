f726039214930d61b9d7c5a1c3f7ea47586cda9a
Svaroglitch <39617666+Svaroglitch@users.noreply.github.com>
2021-05-20 10:48:25 +0200

    Add files via upload

---
+++ Fondreimg.py
@@ -0,0 +1,60 @@
+"""
+Fondre_Img
+Creator: Lehag
+
+Fondre est un outils python permettant de créer une image
+"moyenne" a partir de deux image de même taille
+
+
+
+"""
+from PIL import Image
+import os
+# recuperation des nom de fichier
+inp_dir = r"C:\Users\Lehag\OneDrive\Bureau\FondreImg\input"
+out_dir = r"C:\Users\Lehag\OneDrive\Bureau\FondreImg\Output"
+
+img1 = os.listdir(inp_dir)[0]
+img2 = os.listdir(inp_dir)[1]
+
+# ouverture des fichier
+openimg1 = Image.open(inp_dir + "\\" + img1)
+openimg2 = Image.open(inp_dir + "\\" + img2)
+out_imag = Image.new(openimg1.mode,openimg1.size)
+
+# Iterateur de boucle
+x = 0
+y = 0
+
+# Boucle de fusion des pixel
+
+    # Test de conformité des images
+if openimg1.size == openimg2.size:
+    print("Taille Image : OK " + str(openimg1.size))
+
+
+    while x < openimg1.size[0]:
+        while y < openimg1.size[1]:
+
+            # recuperation des RGB du pixel x,y
+
+            R1 = openimg1.getpixel((x,y))[0]
+            R2 = openimg2.getpixel((x,y))[0]
+            Rmoy = int(round((R1+R2)/2))
+
+            V1 = openimg1.getpixel((x,y))[1]
+            V2 = openimg2.getpixel((x,y))[1]
+            Vmoy = int(round((V1+V2)/2))
+
+            B1 = openimg1.getpixel((x,y))[2]
+            B2 = openimg2.getpixel((x,y))[2]
+            Bmoy = int(round((B1+B2)/2))
+
+            out_imag.putpixel((x,y),(Rmoy,Vmoy,Bmoy))
+
+            y += 1
+        y = 0
+        x += 1
+
+out_imag.save(out_dir +"\\Fusion_img.png","PNG")
+out_imag.show()
